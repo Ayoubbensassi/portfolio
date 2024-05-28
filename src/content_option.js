39d24ef4e6a139825879b1edc24b5b003be449f8
Ayoub bensassi <souspoker3@gmail.com>
2024-05-28 14:17:21 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,113 @@
+const logotext = "BENSASSI";
+const meta = {
+    title: "Ayoub bensassi ",
+    description: "I am a graphic designer with extensive experience in blogging and copywriting, now expanding my skills as a beginner web developer and UI/UX designer.",
+};
+
+const introdata = {
+    title: "Ayoub bensassi",
+    animated: {
+        first: "im into development",
+        second: "Designing cool app",
+        third: "Enjoy ",
+    },
+    description: "I am a full stack developer/ graphic designer with extensive experience in blogging and copywriting",
+    your_img_url: "https://i.ibb.co/J2wQHQY/IMG-4460-2.jpg",
+};
+
+const dataabout = {
+    title: "about me",
+    aboutme: "Experienced graphic designer and copywriter transitioning into web development and UI/UX design, leveraging my skills to create visually appealing, user-centric digital experiences. Passionate about integrating design, content, and functionality for seamless user interaction.",
+};
+const worktimeline = [{
+        jobtitle: "dev/ops ",
+        where: "zinana agency",
+        date: "2020",
+    },
+    {
+        jobtitle: "ui/ux",
+        where: "tofaha",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer ",
+        where: "zinan agency",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 70,
+    },
+
+    {
+        name: "Javascript",
+        value: 40,
+    },
+    {
+        name: "React",
+        value: 40,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I create visually appealing, user-centric UI/UX designs that enhance functionality and user satisfaction.",
+    },
+    {
+        title: "Creating websites",
+        description: "I design and develop visually appealing, user-centric websites that seamlessly integrate design, content, and functionality..",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I specialize in designing and developing user-friendly, visually appealing WordPress websites that integrate seamless functionality and engaging content",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.mediafire.com/view/42qwrayg707x5kn/673a8ca5f23c70c02d5bcf56d1424b9d.png/file",
+        description: "Store.",
+        link: "#",
+    },
+    {
+        img: "https://www.mediafire.com/view/srtc7ss6z0f9lqw/original-0606ea2e576dcf08407fdfc8fd3fefbb.png/file",
+        description: "Financial dashboard.",
+        link: "#",
+    },
+    {
+        img: "https://www.mediafire.com/view/0tqehefuap4n94b/Speedlink-Headset-Product-Header.png/file",
+        description: "Gaming store.",
+        link: "#",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bensassiarts@gmail.com",
+    YOUR_FONE: "+2120652065218",
+    description: "Reach out to me for your design and web development needs by contacting me ",
+  
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Ayoubbensassi",
+    facebook: "https://www.facebook.com/profile.php?id=100052333082128",
+    linkedin: "https://www.linkedin.com/in/ayoubbensassi/",
+    twitter: "https://x.com/elguilibensassi",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
